e99f43cc8acffb97fe360348f8c30a88c7dcb1ce
Enkiggu <58304648+Enkiggu@users.noreply.github.com>
2023-04-14 17:32:05 +0300

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -4,3 +4,6 @@ const dropDownMenu = document.querySelector('#dropdown-menu');
 dropDown.addEventListener('click', () => {
     dropDownMenu.classList.toggle('hidden');
 });
+
+
+ScrollReveal().reveal('.ani1', { delay: 500 });
